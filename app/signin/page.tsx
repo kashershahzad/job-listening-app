719a363643e2767c779a3af90789c635fce76c39
kashershahzad <kashershahzadprogramming@gmail.com>
2025-02-21 21:50:46 +0500

    vercel build update

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Layout } from '@/app/components/auth/layout';
+import Layout from '@/app/components/auth/layout';
 import { SignInForm } from '@/app/components/auth/signin-form';
 
 export default function Page(): React.JSX.Element {
